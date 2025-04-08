b2fefff8405e3ccacefc0f0dc8efde6bd7edf982
Dương Nguyễn <huyduong5112003@gmail.com>
2025-04-08 15:49:03 +0700

    remove file

--- app/routes/cookies.server.ts
+++
@@ -1,8 +0,0 @@
-import { createCookie } from "@remix-run/node"; // or cloudflare/deno
-
-export const userPrefs = createCookie("user-prefs", {
-  maxAge: 300, // one week
-  path: "/",
-  httpOnly: false, // Để có thể truy cập từ JavaScript
-  sameSite: "lax",
-});
